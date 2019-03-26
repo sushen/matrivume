19a6a27952c65ebb54157425e18b0fb7c911d702
dev-fahim <fahim6668@gmail.com>
2019-03-26 03:33:23 -0400

    fixed a silly bug.

--- settings/heroku_prod_settings.py
+++ settings/heroku_prod_settings.py
@@ -1,10 +1,5 @@
-try:
-    from settings.base_settings import *
-    import django_heroku
-except ImportError as exc:
-    raise ImportError(
-        "Couldn't import development settings from settings_module."
-    ) from exc
+import django_heroku
+from settings.base_settings import *
 
 DEBUG = False
 WSGI_APPLICATION = 'matrivume.heroku_wsgi.application'
